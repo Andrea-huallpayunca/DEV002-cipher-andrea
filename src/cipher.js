786045f3c791c97600b4c2d712b426f3db26d1e3
unknown <andreahuallpayuncah@gmail.com>
2023-06-06 16:32:39 -0500

    limpiando código - html -js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,44 @@
-const cipher = {
-  // ...
-};
 
+const cipher = { 
+  encode: function(mover,mensaje){
+
+    let letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let resultado = '';
+    if(mensaje && mover){
+      for (let index = 0; index < mensaje.length; index++) {
+        let extraer = mensaje[index] ;
+        let posición = letras.indexOf(extraer);
+        let nuevaPosicion = (posición+(mover%26))%26;
+
+        resultado += letras[nuevaPosicion];
+      }
+      return resultado;
+    } 
+    throw new TypeError("Ingrese una clave y un mensaje");
+  },
+
+  decode: function(mover,mensaje){
+  
+    let letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let resultado = '';
+    
+    if(mensaje && mover){
+      for (let index = 0; index < mensaje.length; index++) {
+    
+        let extraer = mensaje[index] ;
+        let posición = letras.indexOf(extraer);
+        let nuevaPosicion = (26+(posición-(mover%26)))%26;
+        
+        resultado += letras[nuevaPosicion];
+      }
+      return resultado;
+     } 
+     throw new TypeError("Ingrese una clave y un mensaje");
+   }
+
+
+
+
+}
 export default cipher;
+
